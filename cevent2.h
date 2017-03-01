0cc8e125781f0e48ec4d5ae44b17dae6b4abc3fc
Yehor Popovych <y.popovych@crossroadlabs.xyz>
2017-03-01 21:37:18 +0200

    First version

---
+++ cevent2.h
@@ -0,0 +1,23 @@
+//===--- cevhtp.h  -------------------------------------------------------===//
+//
+//Copyright (c) 2016 Daniel Leping (dileping)
+//
+//This file is part of CEVHTP.
+//
+//PathToRegex is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Lesser General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//PathToRegex is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Lesser General Public License for more details.
+//
+//You should have received a copy of the GNU Lesser General Public License
+//along with PathToRegex.  If not, see <http://www.gnu.org/licenses/>.
+//
+//===----------------------------------------------------------------------===//
+
+#include <event2/event.h>
+#include <event2/thread.h>
